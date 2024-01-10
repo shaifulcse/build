e85e227721bafc82a9bb96c02d6144499bf32fdb
shaifcs <46764783+shaifcs@users.noreply.github.com>
2024-01-10 16:24:04 -0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main
 {
     public static void main( String[] args )
     {
+        // testing comment
         boolean valid = false;
         Scanner scanner = new Scanner(System.in);
         passwordChecker passwordChecker = new passwordChecker();
